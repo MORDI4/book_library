13988cf35051c2388feb7be62a0a4e3aa946b5b9
Michal Mordarski <mmordarski4@gmail.com>
2025-09-14 01:28:29 +0200

    Fix code mistakes

--- library.py
+++ library.py
@@ -1,1 +1,11 @@
-from app import app
+from app import app, db
+from app.models import Authors, Books, Rentals
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Author": Authors,
+        "Book": Books,
+        "Rental": Rentals
+    }
